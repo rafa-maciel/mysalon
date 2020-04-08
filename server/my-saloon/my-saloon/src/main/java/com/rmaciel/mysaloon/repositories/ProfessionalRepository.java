00e7995a9e61a17cb6c756677322f8d45b5a1033
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-08 14:16:17 -0300

    created repository for professional model

---
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/repositories/ProfessionalRepository.java
@@ -0,0 +1,9 @@
+package com.rmaciel.mysaloon.repositories;
+
+import com.rmaciel.mysaloon.models.Professional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessionalRepository extends CrudRepository<Professional, Long>{
+
+}

8cd553b3731d199d4760e68189acd100f8c3ce8c
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-13 17:01:46 -0300

    add cross-domain config to localhost:9000

--- server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/controllers/ProfessionalController.java
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/controllers/ProfessionalController.java
@@ -15,6 +15,7 @@ import com.rmaciel.mysaloon.repositories.ProfessionalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 @RequestMapping("/professionals")
 public class ProfessionalController {

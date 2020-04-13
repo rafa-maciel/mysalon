e3d9e40cf37cf1ba29c7677fb23e597b4cd8dbd9
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-13 17:16:00 -0300

    created actions for create and list all professionals

--- client/src/controllers/ProfessionalController.js
+++ client/src/controllers/ProfessionalController.js
@@ -3,19 +3,60 @@ import ProfessionalsList from "../models/ProfessionalsList";
 import ProfessionalsListView from "../views/ProfessionalsListView";
 import ProfessionalForm from "../forms/ProfessionalForm";
 import ProfessionalFormView from "../views/ProfessionalFormView";
+import Professional from "../models/Professional";
+import ProfessionalService from "../services/ProfessionalService";
+import FieldError from "../forms/FieldError";
 
 export default class ProfessionalController {
     constructor() {
         this._professionalsList = new ModelView(new ProfessionalsList(), 
-            new ProfessionalsListView(document.querySelector('#')), 
-            'add');
+            new ProfessionalsListView(document.querySelector('#professionalList')), 'add');
 
         this._form = new ModelView(new ProfessionalForm(document.querySelector('#professionalForm')),
-            new ProfessionalFormView(document.querySelector('#modalForm')),
-            'clean', 'include');
+            new ProfessionalFormView(document.querySelector('#professionalFormFields')),
+            'clean', 'include', 'addErrors');
+
+        this._service = new ProfessionalService();
+        this.init();
     }
 
-    cleanProfessionalForm() {
-        this._form.clean();
+    init() {
+       this._updateAllProfessionals();
     }
+
+    callFormForNewProfessional() {
+        this._form.clean();
+        this._showFormModal();
+    }
+
+    createProfessional() {
+        let dto = this._form.convertToDTOModel();
+        this._service.createProfessional(dto)
+            .then(professional => {
+                this._professionalsList.add(professional)
+                this._showFormModal(false);
+            })
+            .catch(fieldErrors => {
+                this._form.addErrors(fieldErrors);
+            });
+    }
+
+    editProfessional(id) {
+        console.log(id);
+    }
+
+    _updateAllProfessionals() {
+        this._service.getAllProfessionals()
+            .then(professionalsList => professionalsList.forEach(professional => this._professionalsList.add(professional)));
+    }
+
+    _showFormModal(option=true) {
+        if (option) {
+            $("#modalProfessional").modal("show");
+        } else {
+            $("#modalProfessional").modal("hide");
+        }
+    }
+
+
 }
--- client/src/professional.js
+++ client/src/professional.js
@@ -4,5 +4,16 @@ let controller = new ProfessionalController();
 
 document.querySelector(".btn-modal-professional").addEventListener('click', event => {
     event.preventDefault();
-    $("#modalProfessional").modal("show");
+    controller.callFormForNewProfessional();
+});
+
+document.querySelector("#professionalForm").addEventListener('submit', event => {
+    event.preventDefault();
+    controller.createProfessional();
+
+    document.querySelectorAll(".btn-edit-professional").forEach(element => element.addEventListener('click', function(event) {
+        event.preventDefault();
+        let id = this.dataset["professionalid"];
+        controller.editProfessional(id);
+    }));
 });

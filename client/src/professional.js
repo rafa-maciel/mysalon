23a3f11e235db18e6f2e593588118f5f00562b7b
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-14 16:46:59 -0300

    created listeners for remove button and remove form submit

--- client/src/professional.js
+++ client/src/professional.js
@@ -17,6 +17,24 @@ $('#modalProfessional').on('show.bs.modal', event => {
     if (id) {
         controller.showEditProfessionalForm(id);
     } else {
-        controller.callFormForNewProfessional();
+        controller.showNewProfessionalForm();
     }
+});
+
+$('#modalProfessionalRemove').on('show.bs.modal', event => {
+    let id = $(event.relatedTarget).data('professionalid');
+    let name = $(event.relatedTarget).data('professional');
+        
+    let modal = $("#modalProfessionalRemove");
+
+    modal.find(".professional-name").text(name);
+
+    document.querySelector("#formRemoveProfessional input[name='id']").value = id;
+    
+});
+
+document.querySelector("#formRemoveProfessional").addEventListener('submit', event => {
+    event.preventDefault();
+    let id = document.querySelector("#formRemoveProfessional input[name='id']").value;
+    controller.deleteProfessional(id);
 });

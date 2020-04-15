566767d0aabb44abbcddfa84901713afe52c83ff
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-15 13:19:36 -0300

    created service for customers

---
+++ client/src/services/CustomerService.js
@@ -0,0 +1,24 @@
+import HttpHelper from "../helpers/HttpHelper";
+import Customer from "../models/Customer";
+
+export default class CustomerService {
+    constructor() {
+        this._serverURL = `${SERVICE_URL}/customers`;
+        this._http = new HttpHelper();
+    }
+
+    getCustomers() {
+        let endpoint = this._serverURL;
+        return this._http.get(endpoint)
+            .then(dataArray => dataArray.map(data => this._getCustomerFromData(data)))
+            .catch(error => {
+                console.log(error);
+                throw new Error('Não foi possivel conectar ao servidor');
+            });
+    }
+
+    _getCustomerFromData(data) {
+        return new Customer(data['fullname'], data['residencialPhone'], data['cellphone'], data['indicatedBy'], data['professionalEngagedName']);
+    }
+
+}

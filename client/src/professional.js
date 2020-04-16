c125aa0dab724e5b455365bf4045a1b696152b4b
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-16 12:16:13 -0300

    removed unused lines and comments

--- client/src/professional.js
+++ client/src/professional.js
@@ -2,11 +2,6 @@ import ProfessionalController from "./controllers/ProfessionalController";
 
 let controller = new ProfessionalController();
 
-/* document.querySelector('.btn-modal-professional').addEventListener('click', event => {
-    event.preventDefault();
-    controller.callFormForNewProfessional();
-}); */
-
 document.querySelector('#professionalForm').addEventListener('submit', event => {
     event.preventDefault();
     controller.saveFormProfessional();

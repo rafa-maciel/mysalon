4b0f15b14a923573ac81054623cd2057ee86e42f
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-17 14:48:41 -0300

    added config to support spring data

--- server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/MySaloonApplication.java
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/MySaloonApplication.java
@@ -2,8 +2,10 @@ package com.rmaciel.mysaloon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class MySaloonApplication {
 
 	public static void main(String[] args) {

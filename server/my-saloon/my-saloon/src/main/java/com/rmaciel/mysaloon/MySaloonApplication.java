c92a06188434f75b7e719d095afcff61a0afe4ca
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-23 12:10:39 -0300

    started cache config

--- server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/MySaloonApplication.java
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/MySaloonApplication.java
@@ -2,10 +2,12 @@ package com.rmaciel.mysaloon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class MySaloonApplication {
 
 	public static void main(String[] args) {

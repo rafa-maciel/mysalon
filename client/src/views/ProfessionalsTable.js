7389b00a651714cfa699f5c49924c514b3aad05c
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-27 16:05:01 -0300

    refactored class table list, now it is component from modellist

--- client/src/views/ProfessionalsTable.js
+++ client/src/views/ProfessionalsTable.js
@@ -1,8 +1,8 @@
-import Table from "../components/Table";
+import ModelTable from "../components/ModelTable";
 import Button from "../components/Button"
 import ListenerAction from "../components/ListenerAction"
 
-export default class ProfessionalsTableList extends Table{
+export default class ProfessionalsTable extends ModelTable {
     constructor(parentSelector, editAction, removeAction) {
         super(parentSelector, {
             'id': 'professionalsList',
@@ -11,39 +11,9 @@ export default class ProfessionalsTableList extends Table{
 
         this._editAction = editAction;
         this._removeAction = removeAction;
-        this._professionals = [];
     }
 
-    add(professional) {
-        this._professionals.push(professional);
-        this._appendItem(this._getItemFromProfessional(professional));
-    }
-
-    update(professional) {
-        this._professionals.forEach((item, index) => {
-            if (item.id == professional.id) this._professionals[index] = professional;
-        });
-
-        this._updateLine(professional.id, this._getItemFromProfessional(professional));
-    }
-
-    clean() {
-        this._professionals = [];
-        this._cleanTable();
-    }
-
-    find(id) {
-        return this._professionals.find(professional => professional.id == id);
-    }
-
-    remove(id) {
-        return this._professionals.filter(professional => {
-            if (professional.id != id) return true;
-            this._removeLine(id);
-        });
-    }
-    
-    _getItemFromProfessional(professional) {
+    _createLineTableFromModel(professional) {
         let btnEdit = new Button('Editar', 'btn btn-outline-primary btn-sm', 'button', 
             new ListenerAction('click', () => this._editAction(professional.id)));
         

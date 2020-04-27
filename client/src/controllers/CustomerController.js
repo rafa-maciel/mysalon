583a85a5b4dc7440a823ed5473f687bf3a61080b
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-27 18:23:10 -0300

    refactored items and replaced them for components created as Modal Form and Table

--- client/src/controllers/CustomerController.js
+++ client/src/controllers/CustomerController.js
@@ -1,59 +1,111 @@
 import ProxyModelView from "../helpers/BindProxyModelView";
 import ModelList from "../models/ModelList";
-import CustomerListView from "../views/CustomerListView";
 import CustomerService from "../services/CustomerService";
 import AlertMessage from "../models/AlertMessage";
 import AlertMessageView from "../views/AlertMessageView";
-import CustomerForm from "../forms/CustomerForm";
-import ProfessionalService from "../services/ProfessionalService";
-import CustomerFormView from "../views/CustomerFormView";
-import CustomerFilterForm from "../forms/CustomerFilterForm";
-import CustomerFilterFormView from "../views/CustomerFilterFormView";
+import CustomerFilterForm from "../views/CustomerFilterForm";
+import Modal from "../components/Modal";
+import CustomerForm from "../views/CustomerForm";
+import Customer from "../models/Customer";
+import Button from "../components/Button";
+import ListenerAction from "../components/ListenerAction";
+import CustomersTable from "../views/CustomersTable";
+import ProxyModelComponent from "../helpers/ProxyModelComponent";
+import ConfirmModal from "../components/ConfirmModal";
+import PageableNavigation from "../components/PageableNavigation";
 
 export default class CustomerController {
     constructor() {
-        this._customersList = new ProxyModelView(new ModelList(), 
-            new CustomerListView(document.querySelector('#customerList')),
-            'add', 'remove', 'clean');
-
-        this._form = new ProxyModelView(new CustomerForm(),
-            new CustomerFormView(document.querySelector('#customerFormFields')),
-            'clean', 'include', 'addErrors', 'updateProfessionalsList');
-
-        this._filterForm = new ProxyModelView(new CustomerFilterForm(),
-            new CustomerFilterFormView(document.querySelector("#customerFilter .form-fields")),
-            'updateProfessionalsList');
+        this._service = new CustomerService();
 
         this._message = new ProxyModelView(new AlertMessage(),
             new AlertMessageView(document.querySelector('#alertMessage')), 
             'update');
 
-        this._service = new CustomerService();
+        this._modalConfirmRemove = new ConfirmModal("main", {
+            "id": "modalConfirmRemove",
+            "title": "Remover Cliente", 
+            "buttonLabel": "Remover definitivamente"
+        }, id => {this.deleteCustomer(id)});
+
+        this._modalForm = new Modal('main', {
+            'id': 'mForm',
+            'title': 'Formulário do Cliente',
+            'footer': true
+        });
+
+        this._customerForm = new CustomerForm(this._modalForm.contentSelector);
+
+        this._customers = new ProxyModelComponent(new ModelList(), 
+            new CustomersTable('#customerList', 
+                id => {this._editCustomer(id)}, 
+                id => {this._confirmRemoveCustomer(id)}),
+            'add', 'remove', 'clean');
+
+        this._filter = new CustomerFilterForm('#customerFilter', new ListenerAction('submit', event => {
+            event.preventDefault();
+            this._filterCustomers()
+        }));
+
+        this._pageable = new PageableNavigation('#customerList', {
+            'id': 'customerListPagination', 
+        });
 
         this._init();
     }
 
     _init() {
-        this.updateCustomerList();
+        this._filterCustomers();
+        this._initModalFormButtons();
 
-        /* Update the form with professional list */
-        new ProfessionalService().getAllProfessionals()
-            .then(professionals => {
-                this._form.updateProfessionalsList(professionals);
-                this._filterForm.updateProfessionalsList(professionals);
-        });
+        document.querySelector('.btn-create-customer').addEventListener('click', () => {this._createCustomer()});
 
     }
 
-    updateCustomerList() {
-        this._customersList.clean();
+    _initModalFormButtons() {
+        this._modalForm.updateFooter(
+            new Button('Salvar', 'btn btn-primary btn-lg', 'button', 
+                new ListenerAction('click', () => {this.saveCustomerForm()})));
+    }
 
-        let dataFilter = this._filterForm.getJSONParameters();
+    saveCustomerForm() {
+        let dto = this._customerForm.getCustomerDTO();
+        let savePromisse = dto.id ? this._service.updateCustomer(dto) : this._service.createCustomer(dto);
+
+        savePromisse.then(customer => {
+            this._customers.add(customer);
+            this._message.update(`Os dados do ${customer.fullname} foram salvos com sucesso`,
+                `Dados Salvo`, 
+                'success');
+
+                this._modalForm.hide();
+        }).catch(error => console.log(error));
+    }
+
+    deleteCustomer(id) {
+        this._service.deleteCustomer(id)
+            .then(() => {
+                this._customers.remove(id);
+                this._message.update('Os dados do cliente foram removidos definitivamente',
+                'Cliente removido!', 'info');
+            
+            }).catch(error => {
+                this._message.update(error.message,
+                    'Erro na operação!', 'warning');
+            });
+
+        this._modalConfirmRemove.hide();
+    }
+
+    _filterCustomers() {
+        this._customers.clean();
+
+        let dataFilter = this._filter.getData();
         this._service.getCustomers(dataFilter)
             .then(pageableContent => {
-                this._customersList.updatePageableInfo(pageableContent['pageable']);
+                this._pageable.update(pageableContent['pageable']);
                 pageableContent['customers'].forEach(customer => {
-                    this._customersList.add(customer);
+                    this._customers.add(customer);
                 });
                 this._message.update('',
                     'Lista de Clientes atualizada!', 
@@ -66,86 +118,22 @@ export default class CustomerController {
             });
     }
 
-    showNewCustomerForm() {
-        this._form.clean();
-        this._showFormModal();
+    _createCustomer() {
+        this._customerForm.customer = new Customer();
+        this._modalForm.show();
     }
 
-    showEditCustomerForm(id) {
-        this._service.getCustomerByID(id)
-            .then(customer => {
-                this._form.include(customer);
-
-            })
-            .catch(error => {
-                this._message.update(error, 'Não foi possivel encontrar o cliente solicitado', 'warning');
-                this._showFormModal(false);
-            });
+    _editCustomer(id) {
+        let customer = this._customers.find(id);
+        this._customerForm.customer = customer;
+        this._modalForm.show();
     }
 
-    saveCustomerForm() {
-        let dto = this._form.convertToDTOModel();
-        if (dto.id) {
-            this._updateViewsFromSaveForm(
-                this._service.updateCustomer(dto), 
-                'atualizado'
-            )
-        } else {
-            this._updateViewsFromSaveForm(
-                this._service.createCustomer(dto), 
-                'cadastrado'
-            )
-        }
+    _confirmRemoveCustomer(id) {
+        let customer = this._customers.find(id);
+        this._modalConfirmRemove.update(
+            `Você tem certeza que deseja remover definitivamente o(a) cliente <strong>${customer.fullname}</strong> do sistema?`, 
+            customer.id);
+        this._modalConfirmRemove.show();
     }
-
-    deleteCustomer(id) {
-        this._service.deleteCustomer(id)
-            .then(() => {
-                this._customersList.remove(id);
-                this._message.update('Os dados do cliente foram removidos definitivamente',
-                'Cliente removido!', 'info');
-                this._showRemoveFormModal(false);
-            }).catch(error => {
-                this._message.update(error.message,
-                    'Erro na operação!', 'warning');
-                this._showRemoveFormModal(false);
-            });
-    }
-
-
-    _updateViewsFromSaveForm(promisse, actionLabel) {
-        promisse
-            .then(customer => {
-                this._customersList.add(customer);
-                this._message.update(`${customer.fullname} foi ${actionLabel} com sucesso`,
-                `Cliente ${actionLabel}!`, 
-                'success');
-
-                this._showFormModal(false);
-            })
-            .catch(fieldErrors => {
-                this._form.addErrors(fieldErrors);
-            });
-    }
-
-    _showFormModal(option=true) {
-        if (option) {
-            $("#modalCustomer").modal("show");
-        } else {
-            $("#modalCustomer").modal("hide");
-        }
-    }
-
-    _showRemoveFormModal(option=true) {
-        if (option) {
-            $("#modalCustomerRemove").modal("show");
-        } else {
-            $("#modalCustomerRemove").modal("hide");
-        }
-    }
-
-    
-
-
-    
 }

2dafe5fd1abdfb1a7b5c1b4a045e8a2185068db8
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-27 16:05:17 -0300

    refactored class table list, now it is component from modellist

--- client/src/controllers/ProfessionalController.js
+++ client/src/controllers/ProfessionalController.js
@@ -5,16 +5,21 @@ import AlertMessageView from "../views/AlertMessageView";
 import Modal from '../components/Modal';
 import ProfessionalForm from '../views/ProfessionalForm';
 import Professional from "../models/Professional";
+import ModelList from "../models/ModelList";
 import Button from "../components/Button";
 import ListenerAction from "../components/ListenerAction";
-import ProfessionalsTableList from "../views/ProfessionalsTableList";
+import ProfessionalsTable from "../views/ProfessionalsTable";
 import ConfirmModal from "../components/ConfirmModal";
+import ProxyModelComponent from "../helpers/ProxyModelComponent";
 
 export default class ProfessionalController {
     constructor() {
-        this._professionalTable = new ProfessionalsTableList('#professionalList', 
-            id => {this.editProfessional(id)},
-            id => {this.confirmRemoveProfessional(id)});
+        this._professionals = new ProxyModelComponent(new ModelList(),
+            new ProfessionalsTable('#professionalList', 
+                id => {this.editProfessional(id)},
+                id => {this.confirmRemoveProfessional(id)}),
+            'add', 'remove');
+        
 
         this._message = new ModelView(new AlertMessage(),
             new AlertMessageView(document.querySelector('#alertMessage')), 
@@ -40,24 +45,28 @@ export default class ProfessionalController {
 
     _init() {
         this._updateProfessionalTable();
-        this._modalForm.updateFooter(
-            new Button('Salvar', 'btn btn-primary btn-lg', 'button', 
-                new ListenerAction('click', () => {this.saveProfessionalForm()})));
+        this._initModalFormButtons();
 
         document.querySelector('.btn-create-professional').addEventListener('click', () => {
             this.createProfessional();
         });
     }
 
+    _initModalFormButtons() {
+        this._modalForm.updateFooter(
+            new Button('Salvar', 'btn btn-primary btn-lg', 'button', 
+                new ListenerAction('click', () => {this.saveProfessionalForm()})));
+    }
+
     confirmRemoveProfessional(id) {
-        let professional = this._professionalTable.find(id);
+        let professional = this._professionals.find(id);
         this._modalConfirmRemove.update(`Você tem certeza que deseja remover definitivamente o(a) professional <strong>${professional.name}</strong> do sistema?`, 
             professional.id);
         this._modalConfirmRemove.show();
     }
 
     editProfessional(id) {
-        let professional = this._professionalTable.find(id);
+        let professional = this._professionals.find(id);
         this._professionalForm.professional = professional;
         this._modalForm.show();
     }
@@ -69,41 +78,30 @@ export default class ProfessionalController {
 
     saveProfessionalForm() {
         let dto = this._professionalForm.professional;
-        if(dto.id) {
-            this._service.updateProfessional(dto)
-                .then(professional => {
-                    this._professionalTable.update(professional)
-                    this._modalForm.hide();
-                    this._message.update(`Os dados do(a) ${professional.name} foram atualizados com sucesso`,
-                    'Profissional atualizado!', 
-                     'success');
-                });
-        } else {
-            this._service.createProfessional(dto)
-                .then(professional => {
-                    this._professionalTable.add(professional);
-                    this._modalForm.hide();
-                    this._message.update(`${professional.name} foi cadastrado com sucesso`,
-                        'Profissional Cadastrado!', 
-                        'success');
-            })
-        }
+        let savePromisse = dto.id ? this._service.updateProfessional(dto) : this._service.createProfessional(dto);
+
+        savePromisse.then(professional => {
+            this._professionals.add(professional);
+            this._modalForm.hide();
+            this._message.update('',
+                `Os dados do(a) ${professional.name} foram salvos com sucesso`, 
+                'success');
+        });
     }
    
     deleteProfessional(id) {
         this._service.deteleProfessional(id)
             .then(() => {
                 this._modalConfirmRemove.hide();
-                this._professionalTable.remove(id);
+                this._professionals.remove(id);
                 this._message.update('Os dados do profissional foram removidos definitivamente',
                     'Profissional removido!', 'info');
             })
     }
 
     _updateProfessionalTable() {
-        this._professionalTable.clean();
         this._service.getAllProfessionals()
-            .then(professionalsList => professionalsList.forEach(professional => this._professionalTable.add(professional)));
+            .then(professionalsList => professionalsList.forEach(professional => this._professionals.add(professional)));
         
         this._message.update('A lista de profissionais foi sincronizada com o servidor',
             'Lista de Profissionais atualizada!', 

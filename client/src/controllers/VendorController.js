5cccdc369c321559b65fd95a04bbe7e63d727649
Rafa Maciel <rafael.macielf@gmail.com>
2020-04-27 15:51:01 -0300

    refactored, created components for vendor

--- client/src/controllers/VendorController.js
+++ client/src/controllers/VendorController.js
@@ -3,42 +3,71 @@ import ModelList from '../models/ModelList'
 import VendorService from '../services/VendorService';
 import AlertMessage from '../models/AlertMessage';
 import AlertMessageView from '../views/AlertMessageView';
-import VendorListView from '../views/VendorsListView';
-import VendorFormView from '../views/VendorFormView';
-import VendorForm from '../forms/VendorForm';
+
+import Modal from '../components/Modal';
+import VendorForm from '../views/VendorForm';
+import Vendor from '../models/Vendor';
+import Button from '../components/Button';
+import ListenerAction from '../components/ListenerAction';
+import VendorsTable from '../views/VendorsTable';
+import ProxyModelComponent from '../helpers/ProxyModelComponent';
+import ConfirmModal from '../components/ConfirmModal';
 
 export default class VendorController {
     constructor() {
         this._service = new VendorService();
 
-        this._vendors = new BindProxy(new ModelList(),
-            new VendorListView(document.querySelector('#vendorsList')),
-            'add', 'remove');
-
-        this._form = new BindProxy(new VendorForm(),
-            new VendorFormView(document.querySelector('#vendorForm .fields')),
-            'include', 'clean');
-
         this._message = new BindProxy(new AlertMessage(),
             new AlertMessageView(document.querySelector('#alertMessage')),
             'update');
+
+        this._modalConfirmRemove = new ConfirmModal("main", {
+            "id": "modalConfirmRemove",
+            "title": "Remover Fornecedor", 
+            "buttonLabel": "Remover definitivamente"
+        }, id => {this.delete(id)});
+
+        this._modalForm = new Modal('main', {
+            'id': 'mForm',
+            'title': 'Formulário do Fornecedor',
+            'footer': true
+        });
+
+        this._vendors = new ProxyModelComponent(new ModelList(), 
+            new VendorsTable('#vendorsList', 
+                id => {this._editVendor(id)}, 
+                id => {this._confirmRemoveVendor(id)}), 
+            'add', 'remove');
+
+        this._vendorForm = new VendorForm(this._modalForm.contentSelector);
         
         this._init();
     }
 
     _init() {
         this._updateVendorsList();
+        this._initModalFormButtons();
+
+        document.querySelector('.btn-create-vendor').addEventListener('click', () => {this._createVendor()});
     }
 
-    saveForm() {
-        let dto = this._form.convertToDTOModel();
-        if (dto.id) {
-            this._updateViewsFromSaveForm(this._service.update(dto),
-                'atualizado');
-        } else {
-            this._updateViewsFromSaveForm(this._service.create(dto),
-                'criado');
-        }
+    _initModalFormButtons() {
+        this._modalForm.updateFooter(
+            new Button('Salvar', 'btn btn-primary btn-lg', 'button', 
+                new ListenerAction('click', () => {this.saveVendorForm()})));
+    }
+
+    saveVendorForm() {
+        let dto = this._vendorForm.getVendorDTO();
+        let savePromisse = dto.id ? this._service.update(dto) : this._service.create(dto);
+        
+        savePromisse.then(vendor => {
+            this._vendors.add(vendor);
+            this._message.update(``,
+                    `Os dados do fornecedors foram salvos com sucesso.`,
+                    'success');
+            this._modalForm.hide();
+        });
     }
 
     delete(id) {
@@ -46,40 +75,31 @@ export default class VendorController {
             .then(() => {
                 this._vendors.remove(id);
                 this._message.update('', 'O fornecedor foi removido', 'info');
-                this._showRemoveFormModal(false);
+                
             }).catch(error => {
                 this._message.update(error.message, 'A operação falhou', 'warning');
-                this._showRemoveFormModal(false);
             });
+        
+        this._modalConfirmRemove.hide();
     }
 
-    showNewForm() {
-        this._form.clean();
-        this._showFormModal();
+    _createVendor() {
+        this._vendorForm.vendor = new Vendor();
+        this._modalForm.show();
     }
 
-    showEditForm(id) {
-        this._service.getById(id)
-            .then(vendor => {
-                this._form.include(vendor);
-            }).catch(error => {
-                this._message.update(error.message, 
-                    'Não foi possivel encontrar o fornecedor', 'warning');
-            })
+    _editVendor(id) {
+        let vendor = this._vendors.find(id);
+        this._vendorForm.vendor = vendor;
+        this._modalForm.show();
     }
 
-    _updateViewsFromSaveForm(promisse, actionLabel='criado') {
-        promisse.
-            then(vendor => {
-                this._vendors.add(vendor);
-                this._message.update(`O Fornecedor foi ${actionLabel} com sucesso.`,
-                    `Fornecedor ${actionLabel}!`,
-                    'success');
-                
-                this._showFormModal(false);
-            }).catch(errorFields => {
-                this._form.addErrors(errorFields);
-            });
+    _confirmRemoveVendor(id) {
+        let vendor = this._vendors.find(id);
+        this._modalConfirmRemove.update(
+            `Você tem certeza que deseja remover definitivamente o(a) professional <strong>${vendor.name}</strong> do sistema?`, 
+            vendor.id);
+        this._modalConfirmRemove.show();
     }
 
     _updateVendorsList() {
@@ -90,21 +110,5 @@ export default class VendorController {
             }).catch(error => {
                 this._message.update('Erro ao conectar ao servidor', error.message, 'warning');
             });
-    }
-
-    _showFormModal(option=true) {
-        if (option) {
-            $("#modalVendorForm").modal("show");
-        } else {
-            $("#modalVendorForm").modal("hide");
-        }
-    }
-
-    _showRemoveFormModal(option=true) {
-        if (option) {
-            $("#modalVendorRemove").modal("show");
-        } else {
-            $("#modalVendorRemove").modal("hide");
-        }
     }
 }

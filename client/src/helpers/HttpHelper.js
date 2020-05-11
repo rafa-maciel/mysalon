ad76fe34a3579851bbfaf0ee12be62b82a607ef7
Rafa Maciel <rafael.macielf@gmail.com>
2020-05-11 17:00:22 -0300

    fixed the methods from the api endpoints to use token for authentications

--- client/src/helpers/HttpHelper.js
+++ client/src/helpers/HttpHelper.js
@@ -1,74 +1,70 @@
 import FieldError from "../forms/FieldError";
+import AuthenticationTokenService from "../services/AuthenticationTokenService";
 
 export default class HttpHelper {
-    saveModel(endpoint, method, data) {
-        return new Promise((resolve, reject) => {
-            fetch(endpoint, {
-                method: method,
-                body: data,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(res => {
-                if (!res.ok) {
-                    res.json().then(data => data['errors'])
-                        .then(dataArray => {
-                            reject(dataArray.map(data => new FieldError(data['field'], data['defaultMessage'])))
-                        });
-                } else {
-                    res.json().then(data => {resolve(data)});
-                }
-            });
-        });
+    addToken(type, token) {
+        let data = `${type} ${token}`;
+        window.localStorage.setItem('msaloonwbtoken', data);
     }
 
-    deleteModel(endpoint) {
-        return new Promise((resolve, reject) => {
-            fetch(endpoint, {method: 'DELETE'})
-                .then(res => {
-                    if (res.ok && res.status == 200) {
-                        resolve('Model removed as requested');
-                    } else {
-                        reject(res.statusText);
-                    }
-                }).catch(error => {
-                    reject(error);
-                });
-        });
+    cleanToken() {
+        window.localStorage.setItem('msaloonwbtoken', null);
+    }
+
+    get token() {
+        return window.localStorage.getItem('msaloonwbtoken');
+    }
+
+    post(endpoint, data) {
+        let transactionDetails = this._buildTransactionDetails('POST', data);
+        return this._buildFetchTransaction(endpoint, transactionDetails);
     }
 
     get(endpoint) {
-        return new Promise((resolve, reject) => {
-            fetch(endpoint)
-                .then(res => {
-                    if (res.ok && res.status == 200) {
-                        
-                        res.json().then(data => {resolve(data)});
-                    }else {
-                        
-                        reject(res.statusText);
-                    }
-                }).catch(error => {
-                    reject(error);
-                });
+        let transactionDetails = this._buildTransactionDetails('GET', null);
+        return this._buildFetchTransaction(endpoint, transactionDetails);
+    }
+
+    put(endpoint, data) {
+        let transactionDetails = this._buildTransactionDetails('PUT', data);
+        return this._buildFetchTransaction(endpoint, transactionDetails);
+    }
+
+    delete(endpoint) {
+        let transactionDetails = this._buildTransactionDetails('DELETE', null);
+        return fetch(endpoint, transactionDetails)
+            .then(res => {
+                if (res.ok) {
+                    return true;
+                }
+            
+                throw new Error('A transactional error has been happened.');
+            });
+    }
+
+    _buildTransactionHeaders() {
+        return new Headers({
+            'Content-type': 'Application/json',
+            'Authorization': this.token
         });
     }
 
-    getFiltered(endpoint, bodyData) {
-        let url = `${endpoint}?${bodyData ? bodyData : ''}`;
-        return new Promise((resolve, reject) => {
-            fetch(url)
-                .then(res => {
-                    if (res.ok && res.status == 200) {
-                        
-                        res.json().then(data => {resolve(data)});
-                    }else {
-                        
-                        reject(res.statusText);
-                    }
-                }).catch(error => {
-                    reject(error);
-                });
-        });
+    _buildTransactionDetails(method, data) {
+        return {
+            'method': method,
+            'body': data,
+            'headers': this._buildTransactionHeaders()
+        }
+    }
+
+    _buildFetchTransaction(endpoint, transactionDetails) {
+        return fetch(endpoint, transactionDetails)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                
+                throw new Error('A transactional error has been happened.');
+            });
     }
 }

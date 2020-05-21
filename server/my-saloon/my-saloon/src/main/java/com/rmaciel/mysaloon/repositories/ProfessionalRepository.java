ed8e6b950787203f37fb3d969c5cca2791a1eed9
Rafa Maciel <rafael.macielf@gmail.com>
2020-05-21 11:47:49 -0300

    implemented cache on the repository and retired from controller

--- server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/repositories/ProfessionalRepository.java
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/repositories/ProfessionalRepository.java
@@ -2,8 +2,21 @@ package com.rmaciel.mysaloon.repositories;
 
 import com.rmaciel.mysaloon.models.Professional;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProfessionalRepository extends CrudRepository<Professional, Long>{
+    
+    @Override
+    @Cacheable("allProfessionals")
+    public Iterable<Professional> findAll();
 
+    @Override
+    @CacheEvict(value = "allProfessionals", allEntries = true)
+    public <S extends Professional> S save(S entity);
+
+    @Override
+    @CacheEvict(value = "allProfessionals", allEntries = true)
+    public void delete(Professional entity);
 }

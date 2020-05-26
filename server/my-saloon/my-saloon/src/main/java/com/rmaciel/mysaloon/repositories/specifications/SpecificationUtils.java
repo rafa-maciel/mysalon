2a9f133021c26f2659bfbd46a0b822c3f092bd0e
Rafa Maciel <rafael.macielf@gmail.com>
2020-05-26 15:43:36 -0300

    created utils for localdate and locatime

--- server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/repositories/specifications/SpecificationUtils.java
+++ server/my-saloon/my-saloon/src/main/java/com/rmaciel/mysaloon/repositories/specifications/SpecificationUtils.java
@@ -1,23 +1,20 @@
 package com.rmaciel.mysaloon.repositories.specifications;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Calendar;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.SingularAttribute;
-
-import com.rmaciel.mysaloon.models.Purchase;
 
 public abstract class SpecificationUtils {
 
-    public static Predicate filterByDate(Root<Purchase> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<Calendar> criteria, SingularAttribute<Purchase, Calendar> attribute) {
+    public static Predicate filterByDate(Path<Calendar> path, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<Calendar> criteria) {
         if (criteria.getValue() == null) return null;
 
-        Path<Calendar> path = root.<Calendar> get(attribute);
         switch (criteria.getOperation()) {
             case GREATER_THAN:                    
                 return criteriaBuilder.greaterThanOrEqualTo(path, criteria.getValue());
@@ -36,10 +33,51 @@ public abstract class SpecificationUtils {
         }
     }
 
-    public static Predicate filterByBigDecimal(Root<Purchase> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<BigDecimal> criteria, SingularAttribute<Purchase, BigDecimal> attribute) {
+    public static Predicate filterByLocalDate(Path<LocalDate> path, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<LocalDate> criteria) {
         if (criteria.getValue() == null) return null;
 
-        Path<BigDecimal> path = root.<BigDecimal> get(attribute);
+        switch (criteria.getOperation()) {
+            case GREATER_THAN:                    
+                return criteriaBuilder.greaterThanOrEqualTo(path, criteria.getValue());
+
+            case LESS_THAN:
+                return criteriaBuilder.lessThan(path, criteria.getValue());
+
+            case EQUAL:
+                return criteriaBuilder.equal(path, criteria.getValue());
+
+            case BETWEEN:
+                if (criteria.getValueMax() == null) return null;
+                return criteriaBuilder.between(path, criteria.getValue(), criteria.getValueMax());       
+            default:
+                return null;
+        }
+    }
+
+    public static Predicate filterByLocalTime(Path<LocalTime> path, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<LocalTime> criteria) {
+        if (criteria.getValue() == null) return null;
+
+        switch (criteria.getOperation()) {
+            case GREATER_THAN:                    
+                return criteriaBuilder.greaterThanOrEqualTo(path, criteria.getValue());
+
+            case LESS_THAN:
+                return criteriaBuilder.lessThan(path, criteria.getValue());
+
+            case EQUAL:
+                return criteriaBuilder.equal(path, criteria.getValue());
+
+            case BETWEEN:
+                if (criteria.getValueMax() == null) return null;
+                return criteriaBuilder.between(path, criteria.getValue(), criteria.getValueMax());       
+            default:
+                return null;
+        }
+    }
+
+    public static Predicate filterByBigDecimal(Path<BigDecimal> path, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, SearchCriteria<BigDecimal> criteria) {
+        if (criteria.getValue() == null) return null;
+
         switch (criteria.getOperation()) {
             case GREATER_THAN:                    
                 return criteriaBuilder.greaterThanOrEqualTo(path, criteria.getValue());
